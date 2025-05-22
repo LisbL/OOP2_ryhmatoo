65ed588d865fc8cfe68f0bfe0a9cbf18185b8792
LisbL <LisbethLepp12@gmail.com>
2025-05-22 13:27:04 +0300

    Stseenide vahel liikumine

--- src/main/java/com/example/oop_2ryhmatoo/Mang.java
+++ src/main/java/com/example/oop_2ryhmatoo/Mang.java
@@ -1,56 +1,31 @@
 package com.example.oop_2ryhmatoo;
 
-import javafx.animation.Animation;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 import java.io.IOException;
 
 public class Mang extends Application {
-    private static final int PIKKUS = 600;
-    private static final int LAIUS = 800;
 
     @Override
     public void start(Stage lava) throws IOException {
-        Parent juur = FXMLLoader.load(getClass().getResource("Pealehekülg.fxml"));
-        lava.setTitle("RIP Monke");
+        try {
+            Parent juur = FXMLLoader.load(getClass().getResource("/com/example/oop_2ryhmatoo/Pealehekylg.fxml"));
+            lava.setTitle("RIP Monke");
 
-        StackPane paneel = new StackPane();
-        Canvas canvas = new Canvas(LAIUS, PIKKUS);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-        paneel.getChildren().add(canvas);
-
-        Timeline loop = new Timeline(new KeyFrame(Duration.millis(1000.0/60), e -> update(gc)));
-        loop.setCycleCount(Animation.INDEFINITE);
-        loop.play();
-
-        Scene stseen = new Scene(paneel,LAIUS, PIKKUS);
-        lava.setScene(stseen);
-        lava.show();
-
-        Button stardinupp = new Button("START");
-        stardinupp.setLayoutX(100);
-        stardinupp.setLayoutY(100);
-        paneel.getChildren().add(stardinupp);
+            Scene stseen = new Scene(juur);
+            lava.setScene(stseen);
+            lava.show();
+        }  catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    private void update(GraphicsContext gc) {
-        gc.clearRect(0,0, LAIUS, PIKKUS);
-        gc.setFill(Color.NAVAJOWHITE);
-        gc.fillRect(0, 0, LAIUS, PIKKUS);
-    }
 
     public static void main(String[] args) {
         launch();
--- src/main/java/com/example/oop_2ryhmatoo/stseeniKontrollimine.java
+++ src/main/java/com/example/oop_2ryhmatoo/stseeniKontrollimine.java
@@ -2,9 +2,12 @@ package com.example.oop_2ryhmatoo;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 
 public class stseeniKontrollimine {
@@ -16,11 +19,19 @@ public class stseeniKontrollimine {
 
     }
 
-    public void vahetaKarakterValikusse(ActionEvent event){
-
+    public void vahetaKarakterValikusse(ActionEvent event) throws IOException {
+        Parent juur = FXMLLoader.load(getClass().getResource("Valik.fxml"));
+        lava = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stseen = new Scene(juur);
+        lava.setScene(stseen);
+        lava.show();
     }
 
-    public void vahetaVoitlusesse(ActionEvent event){
-
+    public void vahetaVoitlusesse(ActionEvent event) throws IOException {
+        Parent juur = FXMLLoader.load(getClass().getResource("Voitlus.fxml"));
+        lava = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stseen = new Scene(juur);
+        lava.setScene(stseen);
+        lava.show();
     }
 }
